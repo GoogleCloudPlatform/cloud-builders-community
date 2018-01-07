c2e2964336c4ecb7f3ac8a3af5445e9076d22565
Daz Wilkin <daz.wilkin@gmail.com>
2018-01-07 08:39:34 -0800

    Initial commit

---
+++ google-closure-compiler/examples/helloworld.js
@@ -0,0 +1,10 @@
+// ==ClosureCompiler==
+// @compilation_level SIMPLE_OPTIMIZATIONS
+// @output_file_name default.js
+// ==/ClosureCompiler==
+
+// ADD YOUR CODE HERE
+function hello(name) {
+    alert('Hello, ' + name);
+  }
+hello('New user');
