2e41b4885430f5755dc311c8ce0194a2ff94ba9f
Victor Trac <victor@cloudkite.io>
2018-02-18 15:53:00 -0600

    upgrade helm to 2.8.1 and support regional GKE clusters
    handle projects outside of running project context

--- helm/helm.bash
+++ helm/helm.bash
@@ -2,24 +2,34 @@
 
 # If there is no current context, get one.
 if [[ $(kubectl config current-context 2> /dev/null) == "" ]]; then
-    cluster=$(gcloud config get-value container/cluster 2> /dev/null)
-    zone=$(gcloud config get-value compute/zone 2> /dev/null)
-    project=$(gcloud config get-value core/project 2> /dev/null)
+    # This tries to read environment variables. If not set, it grabs from gcloud
+    cluster=${CLOUDSDK_CONTAINER_CLUSTER:-$(gcloud config get-value container/cluster 2> /dev/null)}
+    region=${CLOUDSDK_COMPUTE_REGION:-$(gcloud config get-value compute/region 2> /dev/null)}
+    zone=${CLOUDSDK_COMPUTE_ZONE:-$(gcloud config get-value compute/zone 2> /dev/null)}
+    project=${GCLOUD_PROJECT:-$(gcloud config get-value core/project 2> /dev/null)}
 
     function var_usage() {
         cat <<EOF
-No cluster is set. To set the cluster (and the zone where it is found), set the environment variables
-  CLOUDSDK_COMPUTE_ZONE=<cluster zone>
+No cluster is set. To set the cluster (and the region/zone where it is found), set the environment variables
+  CLOUDSDK_COMPUTE_REGION=<cluster region> (regional clusters)
+  CLOUDSDK_COMPUTE_ZONE=<cluster zone> (zonal clusters)
   CLOUDSDK_CONTAINER_CLUSTER=<cluster name>
 EOF
         exit 1
     }
 
     [[ -z "$cluster" ]] && var_usage
-    [[ -z "$zone" ]] && var_usage
+    [ ! "$zone" -o "$region" ] && var_usage
 
-    echo "Running: gcloud container clusters get-credentials --project=\"$project\" --zone=\"$zone\" \"$cluster\""
-    gcloud container clusters get-credentials --project="$project" --zone="$zone" "$cluster" || exit
+    if [ -n "$region" ]; then
+      echo "Running: gcloud config set container/use_v1_api_client false"
+      gcloud config set container/use_v1_api_client false
+      echo "Running: gcloud beta container clusters get-credentials --project=\"$project\" --region=\"$region\" \"$cluster\""
+      gcloud beta container clusters get-credentials --project="$project" --region="$region" "$cluster" || exit
+    else
+      echo "Running: gcloud container clusters get-credentials --project=\"$project\" --zone=\"$zone\" \"$cluster\""
+      gcloud container clusters get-credentials --project="$project" --zone="$zone" "$cluster" || exit
+    fi
 fi
 
 echo "Running: helm init --client-only"
