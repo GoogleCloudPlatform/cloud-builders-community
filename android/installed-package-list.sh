fa0f61fcc69dfc3ba5b28f05db73fa2f61027124
Richard Lee <dlackty@gmail.com>
2018-10-20 21:16:31 +0800

    Check executable before script

--- android/installed-package-list.sh
+++ android/installed-package-list.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sdkmanager --list --verbose 2>/dev/null | sed -e '/Installed packages:/d' | sed -n '/Available Packages/!p;//q' | grep '^[a-z].*' | sed -e '/tools/d' >packages.txt
+if which sdkmanager >/dev/null; then
+    sdkmanager --list --verbose 2>/dev/null | sed -e '/Installed packages:/d' | sed -n '/Available Packages/!p;//q' | grep '^[a-z].*' | sed -e '/tools/d' >packages.txt
+else
+    echo "Couldn't find sdkmanager in your PATH"
+    exit 1
+fi
