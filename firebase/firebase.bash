213567779df899a030787a52ad1c11331d0e110d
classhopper <classhopp3r@gmail.com>
2018-10-31 09:21:40 +0200

    updated Dockerfile and firebase.bash

--- firebase/firebase.bash
+++ firebase/firebase.bash
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# export path to the locally installed firebase tools
-export PATH=$PATH:/node_modules/firebase-tools/bin
-
-# run the original firebase
+# # run a supplied firebase command with token 'FIREBASE_TOKEN'
 firebase "$@" --token $FIREBASE_TOKEN
