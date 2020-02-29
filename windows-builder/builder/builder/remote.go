ebc34817ca0c2d3eafc0068bab0b4f2ec2556826
Andrew Poydence <poy@google.com>
2020-02-28 18:23:05 -0700

    windows-builder: copy via GCS bucket (#376)
    This commit adds logic to try and use a bucket to transfer the workspace
    to the Windows VM. It creates an object in the {project-id}_cloudbuild
    bucket and then instructs the Windows VM to use `gsutil` via Powershell
    to download the given object.
    
    This assumes the Windows VM has read permission for the bucket.
    
    If this process fails, it fallsback to the existing mechanism of
    transferring files.
    
    fixes #375

--- windows-builder/builder/builder/remote.go
+++ windows-builder/builder/builder/remote.go
@@ -1,13 +1,16 @@
 package builder
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
+	"cloud.google.com/go/compute/metadata"
 	"github.com/masterzen/winrm"
 	"github.com/packer-community/winrmcp/winrmcp"
 )
@@ -19,17 +22,18 @@ const (
 
 // Remote represents a remote Windows server.
 type Remote struct {
-	Hostname *string
-	Username *string
-	Password *string
+	Hostname   *string
+	Username   *string
+	Password   *string
+	BucketName *string
 }
 
 type BuilderServer struct {
 	ImageUrl *string
-	VPC *string
-	Subnet *string
-	Region *string
-	Zone *string
+	VPC      *string
+	Subnet   *string
+	Region   *string
+	Zone     *string
 }
 
 // Wait for server to be available.
@@ -46,7 +50,12 @@ func (r *Remote) Wait() error {
 }
 
 // Copy workspace from Linux to Windows.
-func (r *Remote) Copy() error {
+func (r *Remote) Copy(inputPath string) error {
+	defer func() {
+		// Flush stdout
+		fmt.Println()
+	}()
+
 	hostport := fmt.Sprintf("%s:5986", *r.Hostname)
 	c, err := winrmcp.New(hostport, &winrmcp.Config{
 		Auth:                  winrmcp.Auth{User: *r.Username, Password: *r.Password},
@@ -61,15 +70,66 @@ func (r *Remote) Copy() error {
 		log.Printf("Error creating connection to remote for copy: %+v", err)
 		return err
 	}
-	err = c.Copy("/workspace", `C:\workspace`)
+
+	// First try to create a bucket and have the Windows VM download it via a
+	// GS URL. If that fails, use the remote copy method.
+	err = r.copyViaBucket(
+		context.Background(),
+		inputPath,
+		`C:\workspace`,
+	)
+	if err == nil {
+		// Successfully copied via GCE bucket
+		log.Printf("Successfully copied data via GCE bucket")
+		return nil
+	}
+
+	log.Printf("Failed to copy data via GCE bucket: %v", err)
+
+	err = c.Copy(inputPath, `C:\workspace`)
 	if err != nil {
 		log.Printf("Error copying workspace to remote: %+v", err)
 		return err
 	}
 
-	// Flush stdout
-	fmt.Println()
 	return nil
+}
+
+func (r *Remote) copyViaBucket(ctx context.Context, inputPath, outputPath string) error {
+	var bucket string
+	if r.BucketName == nil || *r.BucketName == "" {
+		// Cloud Build creates a bucket called <PROJECT-ID>_cloudbuild. Put
+		// the object there.
+		client := metadata.NewClient(http.DefaultClient)
+		projectID, err := client.ProjectID()
+		if err != nil {
+			return fmt.Errorf("metadata.ProjectID: %v", err)
+		}
+		bucket = fmt.Sprintf("%s_cloudbuild", projectID)
+	} else {
+		bucket = *r.BucketName
+	}
+	object := fmt.Sprintf("windows-builder-%d", time.Now().UnixNano())
+
+	gsURL, err := writeZipToBucket(
+		ctx,
+		bucket,
+		object,
+		inputPath,
+	)
+	if err != nil {
+		return err
+	}
+
+	pwrScript := fmt.Sprintf(`
+$ErrorActionPreference = "Stop"
+$ProgressPreference = 'SilentlyContinue'
+gsutil cp %q c:\workspace.zip
+Expand-Archive -Path c:\workspace.zip -DestinationPath c:\workspace -Force
+`, gsURL)
+
+	// Now tell the Windows VM to download it.
+	return r.Run(winrm.Powershell(pwrScript))
 }
 
 // Run a command on the Windows remote.
--- windows-builder/builder/main.go
+++ windows-builder/builder/main.go
@@ -6,20 +6,22 @@ import (
 	"log"
 	"os"
 
-	"./builder"
+	"github.com/GoogleCloudPlatform/cloud-builders-community/windows-builder/builder/builder"
 )
 
 var (
-	hostname = flag.String("hostname", "", "Hostname of remote Windows server")
-	username = flag.String("username", "", "Username on remote Windows server")
-	password = flag.String("password", os.Getenv("PASSWORD"), "Password on remote Windows server")
-	command  = flag.String("command", "", "Command to run on remote Windows server")
+	hostname         = flag.String("hostname", "", "Hostname of remote Windows server")
+	username         = flag.String("username", "", "Username on remote Windows server")
+	password         = flag.String("password", os.Getenv("PASSWORD"), "Password on remote Windows server")
+	command          = flag.String("command", "", "Command to run on remote Windows server")
 	notCopyWorkspace = flag.Bool("not-copy-workspace", false, "If copy workspace or not")
-	image    = flag.String("image", "windows-cloud/global/images/windows-server-2019-dc-for-containers-v20191210", "Windows image to start the server from")
-	network = flag.String("network", "default", "The VPC name to use when creating the Windows server")
-	subnetwork = flag.String("subnetwork", "default", "The Subnetwork name to use when creating the Windows server")
-	region = flag.String("region", "us-central1", "The region name to use when creating the Windows server")
-	zone = flag.String("zone", "us-central1-f", "The zone name to use when creating the Windows server")
+	workspacePath    = flag.String("workspace-path", "/workspace", "The directory to copy data from")
+	workspaceBucket  = flag.String("workspace-bucket", "", "The bucket to copy the directory to. Defaults to {project-id}_cloudbuild")
+	image            = flag.String("image", "windows-cloud/global/images/windows-server-2019-dc-for-containers-v20191210", "Windows image to start the server from")
+	network          = flag.String("network", "default", "The VPC name to use when creating the Windows server")
+	subnetwork       = flag.String("subnetwork", "default", "The Subnetwork name to use when creating the Windows server")
+	region           = flag.String("region", "us-central1", "The region name to use when creating the Windows server")
+	zone             = flag.String("zone", "us-central1-f", "The zone name to use when creating the Windows server")
 )
 
 func main() {
@@ -41,10 +43,10 @@ func main() {
 		ctx := context.Background()
 		bs = &builder.BuilderServer{
 			ImageUrl: image,
-			VPC: network,
-			Subnet: subnetwork,
-			Region: region,
-			Zone: zone,
+			VPC:      network,
+			Subnet:   subnetwork,
+			Region:   region,
+			Zone:     zone,
 		}
 		s = builder.NewServer(ctx, bs)
 		r = &s.Remote
@@ -55,10 +57,11 @@ func main() {
 		log.Fatalf("Error connecting to server: %+v", err)
 	}
 
+	r.BucketName = workspaceBucket
 	// Copy workspace to remote machine
-	if (!*notCopyWorkspace) {
+	if !*notCopyWorkspace {
 		log.Print("Copying workspace")
-		err = r.Copy()
+		err = r.Copy(*workspacePath)
 		if err != nil {
 			log.Fatalf("Error copying workspace: %+v", err)
 		}
